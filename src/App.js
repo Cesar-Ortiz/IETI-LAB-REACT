3e759f4651bfb7075b6cde81ce2153ff3e788e37
Yarit <yarit.villalobos@mail.escuelaing.edu.co>
2021-10-04 22:14:31 -0500

    creando formulario en el componente de App

--- src/App.js
+++ src/App.js
@@ -3,22 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>yayo pro, de los mismos dioses
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <form>
+        <input type="text" placeholder="Task name"/>
+        <button>Create Tak</button>
+      </form>
+    </main>
   );
 }
 
