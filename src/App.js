0c3702f76e52828ff171048ec100905addc16065
Yarit <yarit.villalobos@mail.escuelaing.edu.co>
2021-10-04 22:22:59 -0500

    creando checkbox de forma repetitiva

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ function App() {
         <input type="text" placeholder="Task name"/>
         <button>Create Tak</button>
       </form>
+      <ul>
+        <li><input checked="true" type="checkbox"/>
+        <span>Learn React</span></li>
+        <li><input type="checkbox"/>
+        <span>Learn Hocks</span></li>
+        <li><input type="checkbox"/>
+        <span>Keep on Keeping on</span></li>
+      </ul>
     </main>
   );
 }
